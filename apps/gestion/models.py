383656db623ca32f8e73084c5dddc3a5860ae758
DavidCassiet <davidleonelcassiet@hotmail.com>
2021-08-13 09:53:57 -0300

    creacion de clases

--- apps/gestion/models.py
+++ apps/gestion/models.py
@@ -1,6 +1,8 @@
 from enum import unique
 from django.db import models
+from django.db.models.base import Model
 from django.db.models.fields import Field, NullBooleanField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,23 +11,38 @@ class Usuario(models.Model):
     apellido = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=50, null=False, blank=False)
     contraseña = models.CharField(max_length=50, null=False, blank=False)
+    #creada_en=models.DateTimeField(auto_now_add=True)
+
+class Pregunta(models.Model):
+    descripcion = models.TextField()
+    FechaCrea = models.DateField(auto_now_add=True)
 
 class Partida(models.Model):
     nivel = models.IntegerField()
-    fecha = models.DateField()
-    
-class Pregunta(models.Model):
-    descripcion = models.TextField()
+    fechaIni = models.DateField(auto_now_add=True)
+    progreso = models.FloatField()
+    puntos = models.IntegerField()
+    part_preg= models.ManyToManyField(Pregunta,through="Modelo",blank=True)
+
+    class Meta:
+        db_table = "Partidas"
+        ordering = ["-fechaIni"]
+
+class Modelo(models.Model):
+    partida = models.ForeignKey(Partida, on_delete=models.CASCADE,
+    blank=True,null=True)
+    pregunta = models.ForeignKey(Pregunta, on_delete=models.CASCADE,
+    blank=True,null=True)
+    acierta = models.BooleanField()
 
 class Respuesta(models.Model):
     validacion = models.BooleanField()
     descripcion = models.TextField()
-    pregunta = models.ForeignKey(Pregunta, null=True, blank=True)
+    IdPreg = models.ForeignKey(Pregunta, on_delete=models.CASCADE ,null=True, blank=True)
 
-#Esta clase sale de la relacion pregunta - respuesta (n,n)
+#Esta clase sale de la relacion pregunta - partida (n,n)
 
-class Pregunta_Respuesta(models.Model):
-    validacion = models.BooleanField()
-    id_preg = Field.primary_key=True(null=False, unique=True)
-    id_resp = Field.primary_key=True(null=False, unique=True)
-    pregunta = models.ForeignKey()
+#class Muestra_part_preg(models.Model):
+#    IdPartida= models.ForeignKey(Partida, on_delete=models.CASCADE ,null=True, blank=True)
+#    IdPreg = models.ForeignKey(Pregunta, on_delete=models.CASCADE ,null=True, blank=True)
+#    acierta = models.BooleanField()
