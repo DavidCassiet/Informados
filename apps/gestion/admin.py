8a955029df32318f2880be5250ac25f407e81879
DavidCassiet <davidleonelcassiet@hotmail.com>
2021-08-13 09:54:35 -0300

    panel de admin completo

--- apps/gestion/admin.py
+++ apps/gestion/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from apps.gestion.models import Modelo, Partida, Pregunta, Respuesta, Usuario
 
 # Register your models here.
+
+admin.site.register(Usuario)
+
+admin.site.register(Pregunta)
+
+admin.site.register(Partida)
+
+admin.site.register(Modelo)
+
+admin.site.register(Respuesta)
