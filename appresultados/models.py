a09d0a542359769611aab237a1f05677b150f1c9
Roycho1885 <roygenoff@gmail.com>
2021-09-01 23:10:23 -0300

    Cambio models ranking

--- appresultados/models.py
+++ appresultados/models.py
@@ -7,7 +7,7 @@ from appweb.models import Usuario
 class Resultado(models.Model):
     cuestionario = models.ForeignKey(Cuestionario, on_delete=models.CASCADE)
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    puntaje = models.FloatField()
+    puntaje = models.DecimalField(max_digits=10,decimal_places=2)
 
     def __str__(self):
         return str(self.pk)
